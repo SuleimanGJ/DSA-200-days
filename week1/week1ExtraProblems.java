ded02418c73adc21f178b59b417bbeb10081f40e
suleiman <suleimanosman1023@gmail.com>
2026-05-30 12:29:23 +0300

    added  extra problems

---
+++ week1/week1ExtraProblems.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class week1ExtraProblems {
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    // A. Say Hello With C++
+    // Given a name S. Print "Hello, (name)" without parentheses.
+    // Input
+    // Only one line containing a string S.
+    // Output
+    // Print "Hello, " without quotes, then print name.
+
+    // String name = sc.nextLine();
+    // System.out.println("Hello, " + name);
+
+    
+    }
+}
