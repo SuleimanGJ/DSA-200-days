10e7034fc13bcc853dfb4f5cdca3435a39f19279
suleiman <suleimanosman1023@gmail.com>
2026-06-03 08:42:16 +0300

    prcaticed pattern printing and solved some problems

---
+++ week2/patternProblems.java
@@ -0,0 +1,224 @@
+import java.util.*;
+public class patternProblems {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+    // Star Pattern Printing Programs in Java
+    //    Square Star Pattern
+        //    ****
+        //    ****
+        //    ****
+        //    ****
+
+    // int n = sc.nextInt();
+    // for(int i = 0; i<=n; i++){
+    //     String str = " ";
+    //     for(int j = 0; j <= i; j++){
+    //         str += "*";
+    //     }
+    //     System.out.println(str);
+    // }
+
+    // for(int i = 0; i<=n; i++){
+    //     for(int j = 0; j <= n; j++){
+    //     System.out.print("*");
+    //     }
+    //     System.out.println(" ");
+    // }
+
+
+    //    Hollow Square Star Pattern
+
+        //    ****
+        //    *  *
+        //    *  *
+        //    ****
+    // int n = sc.nextInt();
+    // for(int i = 1; i<=n; i++){
+    //     for(int j = 1; j <= n; j++){
+    //     if(i==1 || i==n || j==1 || j==n){
+    //         System.out.print("*");
+    //     }else {
+    //         System.out.print(" ");
+    //     }
+    //     }
+    //     System.out.println("");
+    // } 
+
+    //    Rhombus Star Pattern
+
+        //    ****
+        //     ****
+        //      ****
+        //       ****
+
+        // int n = sc.nextInt();
+        // for(int i=1; i<=n; i++){
+        //     for(int j= 1; j <= i; j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j=1; j <= n; j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+
+    //    Rectangle Star Pattern
+
+        //    ******
+        //    ******
+        //    ******
+        //    ******
+
+    // int rows = sc.nextInt();
+
+    // for(int i = 0; i<=rows; i++){
+    //     for(int j = 0; j <= rows; j++){
+    //     System.out.print("*");
+    //     }
+    //     System.out.println(" ");
+    // }
+
+    //    Hollow Rectangle Star Pattern
+
+        //    ******
+        //    *    *
+        //    *    *
+        //    ******
+
+    // int rows = sc.nextInt();
+
+    // for(int i = 1; i<=rows; i++){
+    //     for(int j = 1; j <= rows; j++){
+    //     if(i == 1 || i == rows || j ==1 || j == rows){
+    //         System.out.print("*");
+    //     }else {
+    //         System.out.print(" ");
+    //     }
+    //     }
+    //     System.out.println("");
+    // }
+
+    //    Parallelogram Star Pattern
+        //    ******
+        //     ******
+        //      ******
+        //       ******
+
+    // int rows = sc.nextInt();
+
+    // for(int i = 1; i <= rows; i++){
+    //     for(int j=1; j<=i; j++){
+    //         System.out.print(" ");
+    //     }
+    //     for(int j=1; j<= rows; j++){
+    //         System.out.print("*");
+    //     }
+    //     System.out.println();
+    // }
+
+    //    Mirrored Rhombus Star Pattern
+        //    ****
+        //   ****
+        //  ****
+        // ****
+
+    // int rows = sc.nextInt();
+
+    // for(int i = rows; i >= 1; i--){
+    //     for(int j=1; j<=i; j++){
+    //         System.out.print(" ");
+    //     }
+    //     for(int j=1; j<= rows; j++){
+    //         System.out.print("*");
+    //     }
+    //     System.out.println();
+    // }
+
+
+    //    Triangle Star Pattern
+        //    *
+        //    **
+        //    ***
+        //    ****
+
+    // int rows = sc.nextInt();
+    // for(int i = 1; i <= rows; i++){
+    //     for(int j=1; j<= i; j++){
+    //         System.out.print("*");
+    //     }
+    //     System.out.println();
+    // }
+
+    //    Pyramid Star Pattern
+        //       *
+        //      ***
+        //     *****
+        //    *******
+
+    // int rows = sc.nextInt();
+
+    // for(int i = 1; i <= rows; i++){
+    //     for(int j = rows - i; j >= 1; j--){
+    //         System.out.print(" ");
+    //     }
+    //     for(int j=1; j<= i; j++){
+    //         System.out.print(" *");
+    //     }
+    //     System.out.println();
+    // }
+
+
+    //    Hollow Pyramid Star Pattern
+        //       *
+        //      * *
+        //     *   *
+        //    *******
+
+    // int rows = sc.nextInt();
+
+    // for(int i = 1; i <= rows; i++){
+    //     for(int j = 1; j <= rows - i; j++){
+    //         System.out.print(" ");
+    //     }
+    //     for(int j=1; j<= ((2 * i) - 1); j++){
+    //         if( i == rows || j == 1 || j == ((2*i)-1)){
+    //             System.out.print(" *");
+    //         }else {
+    //             System.out.print(" ");
+    //         }
+    //     }
+    //     System.out.println();
+    // }
+
+
+    //    Inverted Pyramid Star Pattern
+        //    *******
+        //     *****
+        //      ***
+        //       *
+
+    // int rows = sc.nextInt();
+
+    // for(int i = rows; i >= 1; i--){
+    //     for(int j = rows - i; j >= 1; j--){
+    //         System.out.print(" ");
+    //     }
+    //     for(int j=1; j<= i; j++){
+    //         System.out.print(" *");
+    //     }
+    //     System.out.println();
+    // }
+
+
+    //    Inverted Hollow Pyramid Star Pattern
+    //    Half Diamond Star Pattern
+    //    Diamond Star Pattern
+
+
+    // Number Pattern Printing Programs Questions in Java
+    // Number Star Mix Pattern Programs in Java
+
+    }
+}
