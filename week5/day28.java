b25c889376e627408bc1648a5eea2a087a1ee592
suleiman <suleimanosman1023@gmail.com>
2026-06-20 12:33:00 +0300

    Started Week5

---
+++ week5/day28.java
@@ -0,0 +1,31 @@
+
+public class day28 {
+    public static void main(Stringj[] args){
+        // Day 28 - flowchart & pseudocode
+        //  programming
+        // is the process of writing insstructions
+        // algorithm
+        // is the step-by-step sets of insstructions to complete tasks
+        // flowchart
+        // is the visual diagram that shows steps of the algorithm using symbol
+
+        // components of flowcharts
+        // 1 oval - terminal(start/end)
+        // 2 rectangle - process
+        // 3 parallelogram - input/output
+        // 4 diamond - decision
+        // 5 arrow - flow line
+        // 6 circle - connector
+
+        // pseudocode
+        // is simple way of writing an algorithm using human language
+
+        // ## Problems - Create pseudocode for the following:
+
+            // 1. Input a year and find whether it is a leap year or not.
+            // 2. Take two numbers and print the sum of both.
+            // 3. Take a number as input and print the multiplication table for it.
+            // 4. Take 2 numbers as inputs and find their HCF and LCM.
+            // 5. Keep taking numbers as inputs till the user enters ‘x’, after that print sum of all.
+    }
+}
