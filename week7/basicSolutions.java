4d973c401520ce975450102e2b12c2c10c2d652a
suleiman <suleimanosman1023@gmail.com>
2026-07-07 15:22:37 +0300

    add pattern solution

--- week7/basicSolutions.java
+++ week7/basicSolutions.java
@@ -57,6 +57,65 @@ public class basicSolutions {
         //     System.out.println();
         // }
 
+
+        // // Right Half
+        // int n = sc.nextInt();
+		// for(int i = 1; i <= n; i++){
+		//     for(int j = 1; j <= i; j++){
+		//         System.out.print("* ");
+		//     }
+		//     System.out.println();
+		// }
+		
+        // // Invderted Right Half
+        // int n = sc.nextInt();
+		// for(int i = n; i >= 1; i--){
+		//     for(int j = 1; j <= i; j++){
+		//         System.out.print("* ");
+		//     }
+		//     System.out.println();
+		// }
+
+        // // Left Half Pyramid
+        // int n = sc.nextInt();
+        // for(int i = 1; i <= n; i++){
+        //     for(int j = 1; j <= n - i; j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j = 1; j <= i; j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        // // Inverted Left Half Pyramid
+        // int n = sc.nextInt();
+        // for(int i = n; i >= 1; i--){
+        //     for(int j = 1; j <= n - i; j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j = 1; j <= i; j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        // // full pyramid
+		// int n = sc.nextInt();
+		// for(int i = 1; i <= n; i++){
+        //     for(int j = 1; j <= n - i; j++){
+        //         System.out.print(" ");
+        //     }
+		//     // for(int j = n - i; j >= 1; j--){
+		//     //     System.out.print(" ");
+		//     // }
+		//     for(int j = 1; j <= i; j++){
+		//         System.out.print(" *");
+		//     }
+
+		//     System.out.println();
+		// }
+
         
     }
 }
